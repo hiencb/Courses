07a889063bda8cec26c26c2f594685b3d75f7d06
hientm87 <hientm@fimplus.vn>
2017-02-19 19:05:42 +0700

    Finish assignment

--- Coursera/Machine Learning/Week 6/machine-learning-ex5/ex5/linearRegCostFunction.m
+++ Coursera/Machine Learning/Week 6/machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -9,8 +9,8 @@ function [J, grad] = linearRegCostFunction(X, y, theta, lambda)
 m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
+% J = 0;
+% grad = zeros(size(theta));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost and gradient of regularized linear 
@@ -18,10 +18,19 @@ grad = zeros(size(theta));
 %
 %               You should set J to the cost and grad to the gradient.
 %
+% disp(X);        disp(size(X));      % 12 x 2
+% disp(theta);    disp(size(theta));  % 2 x 1
+% disp(y);        disp(size(y));      % 12 x 1
+diff = X * theta - y;
+theta1 = theta;
+theta1(1,:) = 0;
+% disp(error);    disp(size(error));  % 12 x 1
+% disp(theta1);   disp(size(theta1)); % 2 x 1
+squaredError = diff .* diff;
+squaredTheta = theta1 .* theta1;
 
-
-
-
+J = (sum(squaredError(:)) + lambda * sum(squaredTheta(:))) / (2 * m);
+grad = (X' * diff + lambda * theta1)/m;
 
 
 
